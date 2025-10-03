bd44251b59a89c745484a751c139d39e9366f3e5
Abhishek Anand <mailtomeabhi15@gmail.com>
2025-10-03 13:19:09 +0530

    add all eslint, prettier, ci and docker changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -60,7 +60,7 @@ function App() {
     fetchTasks();
   };
 
-  // let sangam = 'unused variable here';
+  // let abhishek = 'unused variable here';
 
   return (
     <div className="min-h-screen bg-gray-50">
